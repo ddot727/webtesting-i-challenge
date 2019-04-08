3788721444b72506370038f5283312b427e1509e
ddot727 <ddot727@gmail.com>
2019-04-08 16:43:47 -0400

    wrote repair function and tested it

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,44 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+const superDab = {
+  name: "Super Dab",
+  durability: 43,
+  enhancement: 6
+};
+
+const rockOfTheMilly = {
+  name: "Rock of The Milly",
+  durability: 100,
+  enhancement: 7
+};
+
+const brandNewGuitar = {
+  name: "Brand New Guitar",
+  durability: 68,
+  enhancement: 19
+};
+
+const teacupOfTears = {
+  name: "Teacup of Tears",
+  durability: 12,
+  enhancement: 14
+};
+
+describe("enhancer.js", () => {
+  describe("enhancer", () => {
+    it("should return a new item with the durability === 100", () => {
+      expect(enhancer.repair(superDab)).toEqual({
+        ...superDab,
+        durability: 100
+      });
+      expect(enhancer.repair(rockOfTheMilly)).toEqual({
+        ...rockOfTheMilly,
+        durability: 100
+      });
+    });
+    ///Todos
+    it.todo("should enhance if succeeded");
+    it.todo("should fail to enhance");
+  });
+});
